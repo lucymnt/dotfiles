619fc204fd4c3054f43253bb6e58e96a41a18368
lucymnt <vasquezian671@gmail.com>
2026-04-22 23:38:10 -0700

    make hyprlock lock before suspend

--- system/suspend.nix
+++ system/suspend.nix
@@ -1,29 +1,33 @@
-{ config, pkgs, ... }
-{ 
-  systemd.services."hyprlock-before-sleep" = {
-    description = "Lock screen with hyprlock before suspend";
-    before = [ "sleep.target" ];
-    wantedBy = [ "sleep.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      User = "lucyst";  # run as your user
-      Environment = [
-        "WAYLAND_DISPLAY=wayland-0"
-        "XDG_RUNTIME_DIR=/run/user/1000"
-        "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus"
-      ];
-    ExecStartPre = "${pkgs.systemd}/bin/systemctl thaw user.slice";
-    ExecStart = pkgs.writeShellScript "hyprlock-lock" ''
-      ${pkgs.hyprlock}/bin/hyprlock &
-      timeout=50
-      while [ $timeout -gt 0 ]; do
-        if journalctl -n 20 --no-pager -q | grep -q "Locking session"; then
-          break
-        fi
-        sleep 0.1
-        ((timeout--))
-      done
+{ config, pkgs, ... }:
+let
+  hyprlockHook = pkgs.writeShellScript "hyprlock-hook" ''
+    if [ "$1" = "pre" ]; then
+        systemctl thaw user.slice
+        /run/wrappers/bin/sudo -u lucyst \
+          WAYLAND_DISPLAY=wayland-0 \
+          XDG_RUNTIME_DIR=/run/user/1000 \
+          DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+          ${pkgs.hyprlock}/bin/hyprlock &
+        timeout=100
+        timeout=100
+         while [ $timeout -gt 0 ]; do
+             if ${pkgs.systemd}/bin/journalctl -n 30 --no-pager -q | ${pkgs.gnugrep}/bin/grep -q "Locking session"; then
+                 ${pkgs.coreutils}/bin/sleep 1
+                 break
+             fi
+             ${pkgs.coreutils}/bin/sleep 0.1
+             ((timeout--))
+         done
+    fi
+  '';
+in
+{
+  system.activationScripts.hyprlockSleepHook = {
+    text = ''
+      mkdir -p /etc/systemd/system-sleep
+      ln -sf ${hyprlockHook} /etc/systemd/system-sleep/hyprlock-hook
     '';
-    };
-  }; 
+  };
+
+  security.pam.services.hyprlock = {};
 }
