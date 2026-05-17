ff86c0c5fee46d257c3dcc541267f0826efdd0ee
lucymnt <vasquezian671@gmail.com>
2026-05-17 15:39:38 -0700

    remove reduntant enviroment variables

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -35,7 +35,5 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     LIBVA_DRIVER_NAME = "nvidia";
     GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    WLR_DRM_NO_ATOMIC = "1";
   };
 }
