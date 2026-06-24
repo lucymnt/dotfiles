3f3421a35280dc19b57aacbed6c2a63630e9695a
lucymnt <vasquezian671@gmail.com>
2026-06-23 21:56:48 -0700

    add apps

--- modules/apps.nix
+++ modules/apps.nix
@@ -45,6 +45,9 @@
      ffmpeg
      yt-dlp
      ncdu
+     protonup-qt
+     pcsx2
+     qbittorrent
      (import ../pkgs/alderon-games-launcher.nix { inherit pkgs; })
      (import ../pkgs/hayase.nix { inherit pkgs; })
      (import ../pkgs/helium-browser.nix { inherit pkgs; })
